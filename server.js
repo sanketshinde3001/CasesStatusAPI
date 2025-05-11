c0e94458fc02aba70daf08cb506ed0458aed2da0
sanketshinde3001 <sanketshindedu2004@gmail.com>
2025-05-11 15:55:32 +0530

    port mapping

--- server.js
+++ server.js
@@ -191,7 +191,7 @@ async function getCaseDetailsProcess(diaryNo, year) {
 
 // --- Express API Setup ---
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
